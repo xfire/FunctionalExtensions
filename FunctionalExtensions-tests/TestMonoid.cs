ff6eff3ddbd8a8576c70901fe812b3027f115aaf
Rico Schiekel <fire@downgra.de>
2012-01-25 22:08:30 +0100

    add monoid implementation

---
+++ FunctionalExtensions-tests/TestMonoid.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace System.FunctionalExtensions.Tests
+{
+    [TestFixture]
+    class TestMonoid
+    {
+        [Test]
+        public void CompareTwoOrderingFunctionsWithOrderingMonoid()
+        {
+            Func<string, string, int> direct = (x, y) => x.CompareTo(y);
+            Func<string, string, int> byLength = (x, y) => x.Length.CompareTo(y.Length);
+
+            Func<string, string, int> f = (x, y) => Monoids.Ordering.Mappend(byLength(x, y), direct(x, y));
+
+            Assert.That(f("zen", "ant"), Is.GreaterThan(0));
+            Assert.That(f("zen", "ants"), Is.LessThan(0));
+        }
+    }
+}
---
+++ FunctionalExtensions/System/FunctionalExtensions/Monoid.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace System.FunctionalExtensions
+{
+    public interface Monoid<T>
+    {
+        T Mempty { get; }
+        T Mappend(T a, T b);
+        T Mconcat(IEnumerable<T> xs);
+    }
+
+    public static class Monoids
+    {
+        public static Monoid<T> Create<T>(T mempty, Func<T, T, T> mappend)
+        {
+            return new NormalMonoid<T>(mempty, mappend);
+        }
+
+        public static Monoid<int> IntAdd = Create(0, (x, y) => x + y);
+        public static Monoid<int> IntMul = Create(1, (x, y) => x * y);
+        public static Monoid<string> String = Create("", (x, y) => x + y);
+        public static Monoid<bool> All = Create(true, (x, y) => x && y);
+        public static Monoid<bool> Any = Create(false, (x, y) => x || y);
+        public static Monoid<int> Ordering = Create(0, (x, y) => (x != 0) ? x : y);
+
+        public static Monoid<IEnumerable<T>> Enumerable<T>()
+        {
+            return Create(Enumerable.Empty<T>(), (xs, ys) => xs.Concat(ys));
+        }
+        public static Monoid<Option<T>> Option<T>()
+        {
+            return Create(Option.None<T>(), (x, y) => x.IsNone ? y : x);
+        }
+    }
+
+    class NormalMonoid<T> : Monoid<T>
+    {
+        private readonly T _mempty;
+        private readonly Func<T, T, T> _mappend;
+
+        public NormalMonoid(T mempty, Func<T, T, T> mappend)
+        {
+            _mempty = mempty;
+            _mappend = mappend;
+        }
+
+        public T Mempty { get { return _mempty; } }
+
+        public T Mappend(T a, T b)
+        {
+            return _mappend(a, b);
+        }
+
+        public T Mconcat(IEnumerable<T> xs)
+        {
+            return xs.Aggregate(_mempty, _mappend);
+        }
+    }
+}
